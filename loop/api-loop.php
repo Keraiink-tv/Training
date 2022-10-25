e4428dc860a2de9a251ef6d8b48cc1c22f442d19
Monica Somera Jensen <80958891+Keraiink-tv@users.noreply.github.com>
2022-10-25 09:22:47 +0200

    loop

---
+++ loop/api-loop.php
@@ -0,0 +1,11 @@
+<?php
+
+// In this exercise I will try and make a loop
+// first of all you will do a: for
+
+for ($x = 0; $x <= 100; $x++) {
+    echo "Monica";
+}
+
+// If there is a success it will show the name 100 times
+
